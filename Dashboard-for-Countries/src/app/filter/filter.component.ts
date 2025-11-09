ffdc94a445cb3c5efdd622d2cd8012dd2f2965dc
SalmaWaleeddd <salmawaleed2030@outlook.com>
2025-11-09 23:56:47 +0200

    implemented filter feature

---
+++ Dashboard-for-Countries/src/app/filter/filter.component.ts
@@ -0,0 +1,27 @@
+import { Component, effect, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-filter',
+  imports: [FormsModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css'
+})
+export class FilterComponent {
+
+  searchTerm = model('');
+  selectedRegion = model('');
+
+
+  regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  constructor() {
+    /* effect(() => {
+      console.log('Search Term from filter comp:', this.searchTerm());
+      
+    }); */
+
+
+
+  }
+}
