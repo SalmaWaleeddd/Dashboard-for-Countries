604b85106c750696937bbb3c12c26ce52aac3206
SalmaWaleeddd <salmawaleed2030@outlook.com>
2025-11-10 16:54:43 +0200

    Use RouterOutlet

--- Dashboard-for-Countries/src/app/app.component.ts
+++ Dashboard-for-Countries/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HomePageComponent } from './home-page/home-page.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HomePageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
